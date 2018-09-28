4d0dc3b122e4c36a23e33657e203ec420a98aaa0
honghengqiang <honghengqiang@gmail.com>
2018-09-28 18:27:23 +0800

    增加图片文字

--- apps/front/views.py
+++ apps/front/views.py
@@ -3,7 +3,7 @@ import os
 from werkzeug.utils import secure_filename
 from .forms import UpLoadForm
 from werkzeug.datastructures import CombinedMultiDict
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
 bp = Blueprint('front', __name__)
 UPLOAD_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'static/ima')
@@ -53,6 +53,48 @@ def rotate():
         return jsonify({"filename": url})
 
 
+@bp.route('/change')
+def change():
+    url = "change.png"
+    filename = request.args.get('filename')
+    name = request.args.get('title')
+    text = list(name)
+    # pic2Text(filename, text)
+
+    img = Image.open(UPLOAD_PATH + '/' + filename).convert("RGBA")
+    w = 100  # 宽度固定
+    h = int((float(img.size[1]) / float(img.size[0])) * w)  # 高度按比例得出
+    fontSize = 20  # 输出字体大小
+    img.thumbnail((w, h))  # 获取缩略图
+    src = img.convert('L')  # 转换成灰度图
+    minGrey = 255  # 最小灰度
+    maxGrey = 0  # 最大灰度
+    greyMap = [[0 for col in range(h)] for row in range(w)]  # 灰度图 注意 w h
+    # 此处循环求得灰度表以及最大最小灰度值
+    for i in range(w):
+        for j in range(h):
+            greyMap[i][j] = src.getpixel((i, j))  # 获取每一个点的灰度值
+            if greyMap[i][j] > maxGrey:  # 获取最大灰度
+                maxGrey = greyMap[i][j]
+            if greyMap[i][j] < minGrey:  # 获取最小灰度
+                minGrey = greyMap[i][j]
+    # 计算灰度间隔
+    greyStep = (maxGrey - minGrey) / len(text)
+    # 此处生成文字图片,注意输出的时候 w 和 h
+    output = Image.new('RGBA', (w * fontSize, h * fontSize), (255, 255, 255))
+    draw = ImageDraw.Draw(output)
+    ft = ImageFont.truetype(UPLOAD_PATH + "/msyhbd.ttf", fontSize)  # 注意字体支持中文
+    for j in range(h):
+        for i in range(w):
+            index = int((greyMap[i][j] - minGrey) // greyStep)  # 计算出改点使用哪个字符
+            if index >= len(text):
+                index = len(text) - 1  # 注意结尾最大灰度值，防止越界
+            draw.text((i * fontSize, j * fontSize), text[index], fill=img.getpixel((i, j)), font=ft)  # 汉字编码
+        # output.save(file.split('.')[0] + '_text.png', 'PNG')
+    output.save(UPLOAD_PATH + "/" + url, 'PNG')
+    return jsonify({"filename": url})
+
+
 @bp.route('/tongdao')
 def tongdao():
     filename = request.args.get('filename')
@@ -77,7 +119,6 @@ array = [ImageFilter.BLUR, ImageFilter.CONTOUR, ImageFilter.DETAIL,
 
 @bp.route('/filter')
 def filter():
-    print('dfg fg ')
     global i, filename
     if i == 0:
         filename = request.args.get('filename')
--- static/js/rotate.js
+++ static/js/rotate.js
@@ -88,3 +88,41 @@ $(function () {
         });
     });
 });
+
+$(function () {
+    $("#change").click(function () {
+        var loading = $('#fountainG');
+        loading.show();
+        var text_input = $("input[name='title']");
+        var title = text_input.val();
+        var filename = $('.img01').attr('src');
+        var array = filename.split('/');
+        var name = array[array.length - 1];
+        var b = name.indexOf('?') != -1;
+        var ss = name;
+        console.log("title:" + title);
+        if (b) {
+            var dd = name.split('?');
+            ss = dd[0];
+        } else {
+            ss = name;
+        }
+        zlajax.get({
+
+            'url': '/change',
+            'data': {
+                title: title,
+                filename: ss
+            },
+            'success': function (data) {
+                /*后面加个随机数是方便页面刷新*/
+                loading.hide();
+                $('.img01').attr('src', '../../static/ima/' + data['filename'] + "?t=" + Math.random());
+            },
+            'fail': function (error) {
+                loading.hide();
+                console.log(error);
+            }
+        });
+    });
+});
